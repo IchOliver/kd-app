7b612ee198aa75cda99d33bace851b266bff1aed
MacPc <oliver2america@gmail.com>
2020-11-15 23:16:54 -0800

    stand page

---
+++ src/screens/stand/dist/stand-list.dev.js
@@ -0,0 +1,41 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.StandList = void 0;
+
+var _images = _interopRequireDefault(require("../../constants/images"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var StandList = [{
+  image: _images["default"].Logo,
+  title: 'Deloitte',
+  desc: '35'
+}, {
+  image: _images["default"].Logo,
+  title: 'Lyse',
+  desc: '35'
+}, {
+  image: _images["default"].Logo,
+  title: 'Deloitte',
+  desc: '35'
+}, {
+  image: _images["default"].Logo,
+  title: 'Lyse',
+  desc: '35'
+}, {
+  image: _images["default"].Logo,
+  title: 'Deloitte',
+  desc: '35'
+}, {
+  image: _images["default"].Logo,
+  title: 'Lyse',
+  desc: '35'
+}, {
+  image: _images["default"].Logo,
+  title: 'Deloitte',
+  desc: '35'
+}];
+exports.StandList = StandList;
