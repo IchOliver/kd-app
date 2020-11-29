941cf846cd9356b81dcca42808194705e8d0d969
MacPc <oliver2america@gmail.com>
2020-11-28 23:25:40 -0800

    added config

---
+++ src/config/dist/index.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.BASE_IMAGE_URL = exports.API_URL = void 0;
+var API_URL = 'http://kduiaapp.com/public/api';
+exports.API_URL = API_URL;
+var BASE_IMAGE_URL = "http://kduiaapp.com/public/";
+exports.BASE_IMAGE_URL = BASE_IMAGE_URL;
