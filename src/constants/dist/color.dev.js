af66892f3bbbf37515e20cbc577a0c64c3205ac9
MacPc <oliver2america@gmail.com>
2020-11-30 23:26:56 -0800

    constant

---
+++ src/constants/dist/color.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var Colors = {
+  Blue: '#22A6EB',
+  Black: '#000000',
+  White: '#ffffff',
+  Grey: '#f4f4f4',
+  Purp: '#C24B34',
+  Purple: '#25405F'
+};
+var _default = Colors;
+exports["default"] = _default;
---
+++ src/constants/dist/images.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var Images = {
+  Logo: require('../assets/images/Logo.png'),
+  Map: require('../assets/images/map.png')
+};
+var _default = Images;
+exports["default"] = _default;
